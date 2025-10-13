82590cbc22efdb09f855f5a59de78228aae58de9
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-10-13 15:49:03 -0600

    Servicio del blog y operaciones CRUD

---
+++ src/api/services/blogContentService.js
@@ -0,0 +1,11 @@
+import { API_ENDPOINTS } from "../../constants/apiEndpoints";
+import httpClient from "../httpClient";
+
+
+const blogContentService = {
+    blogContent: () => httpClient.get(API_ENDPOINTS.BLOGCONTENT.GETBLOGCONTENT),
+    register: (formData) => httpClient.post(API_ENDPOINTS.BLOGCONTENT.REGISTER, formData),
+    delete: (id) => httpClient.delete(`${API_ENDPOINTS.BLOGCONTENT.DELETE}${id}`)
+}
+
+export default blogContentService;
