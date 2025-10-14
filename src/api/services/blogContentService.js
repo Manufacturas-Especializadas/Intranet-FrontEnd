9789d35430c77ecc2898b3bb978d886b54c98735
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-10-14 10:11:31 -0600

    Endpoint devuelve informacion dependiendo de el tipo de pagina

--- src/api/services/blogContentService.js
+++ src/api/services/blogContentService.js
@@ -3,7 +3,9 @@ import httpClient from "../httpClient";
 
 
 const blogContentService = {
-    blogContent: () => httpClient.get(API_ENDPOINTS.BLOGCONTENT.GETBLOGCONTENT),
+    blogContent: (pageType) => {
+        return httpClient.get(`${API_ENDPOINTS.BLOGCONTENT.GETBLOGCONTENT}?pageType=${encodeURIComponent(pageType)}`);
+    },
     register: (formData) => httpClient.post(API_ENDPOINTS.BLOGCONTENT.REGISTER, formData),
     delete: (id) => httpClient.delete(`${API_ENDPOINTS.BLOGCONTENT.DELETE}${id}`)
 }
