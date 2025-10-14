55a2a1b5b72a3d99a920f10b51b1df40e9750ed8
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-10-14 11:04:18 -0600

    Se quito la extension del archivo

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { FaIdCard, FaUser, FaLock, FaUserTag, FaBuilding } from "react-icons/fa";
 import Swal from "sweetalert2";
-import authService from "../../api/services/authService.JS";
+import authService from "../../api/services/authService";
 
 const Register = () => {
     const [formData, setFormData] = useState({
