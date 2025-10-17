d05c6402611856097f253dbdde4408c6314648ab
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-10-17 11:14:40 -0600

    Boton para editar informacion

--- src/components/DynamicComponents/DynamicSection.jsx
+++ src/components/DynamicComponents/DynamicSection.jsx
@@ -1,20 +1,30 @@
 import HeaderTitle from "../HeaderTitle/HeaderTitle";
-import { FaBullhorn, FaTrash } from "react-icons/fa";
+import { FaBullhorn, FaPencilAlt, FaTrash } from "react-icons/fa";
 import { RoleGuard } from "../RoleGuard/RoleGuard";
 
-export const DynamicSection = ({ id, title, subTitle, description, content, imageUrl, template, onDelete }) => {
+export const DynamicSection = ({ id, title, subTitle, description, content, imageUrl, template, onDelete, onEdit }) => {
     if (template === "imagen" && imageUrl) {
         return (
             <section className="min-h-[550px] flex items-center bg-gray-100 py-12 sm:py-0 relative">
-
                 <RoleGuard allowedRoles={["Admin", "Recursos humanos", "Calidad", "TI", "Manufactura", "A&T"]}>
-                    <button
-                        onClick={() => onDelete(id)}
-                        className="absolute top-4 right-4 text-gray-400 hover:text-red-500 transition-colors p-2 rounded-full
-                        z-20 hover:cursor-pointer"
-                    >
-                        <FaTrash size={20} />
-                    </button>
+                    <div className="absolute top-4 left-4 z-20 flex gap-2">
+                        <button
+                            className="text-gray-400 hover:text-blue-500 transition-colors 
+                            p-2 rounded-full hover:cursor-pointer"
+                            onClick={() => onEdit(id)}
+                            title="Editar contenido"
+                        >
+                            <FaPencilAlt size={20} />
+                        </button>
+                        <button
+                            onClick={() => onDelete(id)}
+                            className="text-gray-400 hover:text-red-500 transition-colors 
+                            p-2 rounded-full hover:cursor-pointer"
+                            title="Eliminar contenido"
+                        >
+                            <FaTrash size={20} />
+                        </button>
+                    </div>
                 </RoleGuard>
                 <div className="contianer mx-auto px-4">
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
@@ -48,13 +58,26 @@ export const DynamicSection = ({ id, title, subTitle, description, content, imag
         return (
             <section className="min-h-[550px] flex items-center bg-gray-100 py-12 sm:py-0 relative">
                 <RoleGuard allowedRoles={["Admin", "Recursos humanos", "Calidad", "TI", "Manufactura", "A&T"]}>
-                    <button
-                        onClick={() => onDelete(id)}
-                        className="absolute top-4 right-4 text-gray-400 hover:text-red-500 transition-colors p-2 rounded-full z-20 hover:cursor-pointer"
-                    >
-                        <FaTrash size={20} />
-                    </button>
+                    <div className="absolute top-4 left-4 z-20 flex gap-2">
+                        <button
+                            className="text-gray-400 hover:text-blue-500 transition-colors 
+                            p-2 rounded-full hover:cursor-pointer"
+                            onClick={() => onEdit(id)}
+                            title="Editar contenido"
+                        >
+                            <FaPencilAlt size={20} />
+                        </button>
+                        <button
+                            onClick={() => onDelete(id)}
+                            className="text-gray-400 hover:text-red-500 transition-colors 
+                            p-2 rounded-full hover:cursor-pointer"
+                            title="Eliminar contenido"
+                        >
+                            <FaTrash size={20} />
+                        </button>
+                    </div>
                 </RoleGuard>
+
                 <div className="container mx-auto px-4">
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
                         <div className="order-2 md:order-1 flex justify-center">
@@ -87,6 +110,26 @@ export const DynamicSection = ({ id, title, subTitle, description, content, imag
         <section>
             <div className="max-w-full mx-auto">
                 <div className="relative rounded-xl shadow-lg transition-shadow hover:shadow-xl overflow-hidden p-8 md:p-12 text-center">
+                    <RoleGuard allowedRoles={["Admin", "Recursos humanos", "Calidad", "TI", "Manufactura", "A&T"]}>
+                        <div className="absolute top-4 left-4 z-20 flex gap-2">
+                            <button
+                                className="text-gray-400 hover:text-blue-500 transition-colors 
+                            p-2 rounded-full hover:cursor-pointer"
+                                onClick={() => onEdit(id)}
+                                title="Editar contenido"
+                            >
+                                <FaPencilAlt size={20} />
+                            </button>
+                            <button
+                                onClick={() => onDelete(id)}
+                                className="text-gray-400 hover:text-red-500 transition-colors 
+                            p-2 rounded-full hover:cursor-pointer"
+                                title="Eliminar contenido"
+                            >
+                                <FaTrash size={20} />
+                            </button>
+                        </div>
+                    </RoleGuard>
                     <FaBullhorn className="absolute -top-4 -right-4 text-gray-100 text-9xl transform rotate-12 -z-0" />
 
                     <div className="relative z-10">
