ac08912ff4fa70b02616508d25fd312846bebb9d
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-08 07:17:16 -0600

    Codigo formatado

--- src/pages/Manufactura/Manufactura.jsx
+++ src/pages/Manufactura/Manufactura.jsx
@@ -1,4 +1,4 @@
-import Hero from "../../components/ManufacturaComponentes/Hero/Hero"
+import Hero from "../../components/ManufacturaComponentes/Hero/Hero";
 import Swal from "sweetalert2";
 import blogContentService from "../../api/services/blogContentService";
 import { useEffect, useState } from "react";
@@ -10,133 +10,126 @@ import { EditSectionModal } from "../../components/DynamicComponents/EditSection
 import { LoadingSpinner } from "../../components/LoadingSpinner/LoadingSpinner";
 
 const Manufactura = () => {
-    const [sections, setSections] = useState([]);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-    const [editingId, setEditingId] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
+  const [sections, setSections] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [editingId, setEditingId] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const getBlobContent = async () => {
+  const getBlobContent = async () => {
+    try {
+      const response = await blogContentService.blogContent("Manufactura");
+      setSections(response.data);
+    } catch (error) {
+      console.error("Error al obtener la data", error);
+      setSections([]);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getBlobContent();
+  }, []);
+
+  const handleSectionAdded = () => {
+    setIsModalOpen(false);
+    getBlobContent();
+  };
+
+  const handleEditSection = (id) => {
+    setEditingId(id);
+    setIsEditModalOpen(true);
+  };
+
+  const handleSectionEdited = () => {
+    setIsEditModalOpen(false);
+    getBlobContent();
+  };
+
+  const handleDeleteSection = async (id) => {
+    Swal.fire({
+      title: "¿Estás seguro?",
+      text: "¡No podrás revertir esta acción!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí, ¡eliminar!",
+      cancelButtonText: "Cancelar",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
         try {
-            const response = await blogContentService.blogContent("Manufactura");
-            setSections(response.data);
+          await blogContentService.delete(id);
+          getBlobContent();
+          Swal.fire("¡Eliminado!", "La sección ha sido eliminada.", "success");
         } catch (error) {
-            console.error("Error al obtener la data", error);
-            setSections([]);
-        } finally {
-            setIsLoading(false);
+          console.error("Error al eliminar la sección: ", error);
+          Swal.fire({
+            title: "Error",
+            text: "No se pudo eliminar la sección. Inténtalo de nuevo",
+            icon: "error",
+          });
         }
-    };
+      }
+    });
+  };
 
-    useEffect(() => {
-        getBlobContent()
-    }, []);
+  const renderContent = () => {
+    if (isLoading) {
+      return <LoadingSpinner />;
+    }
 
-    const handleSectionAdded = () => {
-        setIsModalOpen(false);
-        getBlobContent();
-    };
+    if (sections.length === 0) {
+      return (
+        <div className="text-center py-20">
+          <p className="text-lg text-gray-500">No hay secciones para mostrar</p>
+        </div>
+      );
+    }
 
-    const handleEditSection = (id) => {
-        setEditingId(id);
-        setIsEditModalOpen(true);
-    };
+    return sections.map((section) => (
+      <DynamicSection
+        key={section.id}
+        id={section.id}
+        onEdit={handleEditSection}
+        onDelete={handleDeleteSection}
+        title={section.title}
+        subTitle={section.subTitle}
+        description={section.description}
+        content={section.content}
+        imageUrl={section.img}
+        template={section.template}
+      />
+    ));
+  };
 
-    const handleSectionEdited = () => {
-        setIsEditModalOpen(false);
-        getBlobContent();
-    };
+  return (
+    <>
+      <Hero />
 
-    const handleDeleteSection = async (id) => {
-        Swal.fire({
-            title: '¿Estás seguro?',
-            text: "¡No podrás revertir esta acción!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Sí, ¡eliminar!',
-            cancelButtonText: 'Cancelar'
-        }).then(async (result) => {
-            if (result.isConfirmed) {
-                try {
-                    await blogContentService.delete(id);
-                    getBlobContent();
-                    Swal.fire('¡Eliminado!', 'La sección ha sido eliminada.', 'success');
+      <main className="max-w-full mx-auto space-y-2">{renderContent()}</main>
 
-                } catch (error) {
-                    console.error("Error al eliminar la sección: ", error);
-                    Swal.fire({
-                        title: "Error",
-                        text: "No se pudo eliminar la sección. Inténtalo de nuevo",
-                        icon: "error"
-                    });
-                }
-            }
-        });
-    };
+      <RoleGuard allowedRoles={["Admin", "Recursos humanos", "Capacitación"]}>
+        <AddSectionButton onClick={() => setIsModalOpen(true)} />
+      </RoleGuard>
 
-    const renderContent = () => {
-        if (isLoading) {
-            return <LoadingSpinner />
-        }
+      <AddSectionModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onAdd={handleSectionAdded}
+        pageType="Manufactura"
+      />
 
-        if (sections.length === 0) {
-            return (
-                <div className="text-center py-20">
-                    <p className="text-lg text-gray-500">
-                        No hay secciones para mostrar
-                    </p>
-                </div>
-            )
-        }
+      <EditSectionModal
+        isOpen={isEditModalOpen}
+        onClose={() => setIsEditModalOpen(false)}
+        onEdit={handleSectionEdited}
+        id={editingId}
+        pageType="Manufactura"
+      />
+    </>
+  );
+};
 
-        return sections.map(section => (
-            <DynamicSection
-                key={section.id}
-                id={section.id}
-                onEdit={handleEditSection}
-                onDelete={handleDeleteSection}
-                title={section.title}
-                subTitle={section.subTitle}
-                description={section.description}
-                content={section.content}
-                imageUrl={section.img}
-                template={section.template}
-            />
-        ))
-    };
-
-    return (
-        <>
-            <Hero />
-
-            <main className="max-w-full mx-auto space-y-2">
-                {
-                    renderContent()
-                }
-            </main >
-
-            <RoleGuard allowedRoles={["Admin", "Recursos humanos", "Capacitación"]}>
-                <AddSectionButton onClick={() => setIsModalOpen(true)} />
-            </RoleGuard>
-
-            <AddSectionModal
-                isOpen={isModalOpen}
-                onClose={() => setIsModalOpen(false)}
-                onAdd={handleSectionAdded}
-                pageType="Manufactura"
-            />
-
-            <EditSectionModal
-                isOpen={isEditModalOpen}
-                onClose={() => setIsEditModalOpen(false)}
-                onEdit={handleSectionEdited}
-                id={editingId}
-                pageType="Manufactura"
-            />
-        </>
-    )
-}
-
-export default Manufactura
+export default Manufactura;
