5e9e0de2f8e3b69eb1ff2119f00ed37b16f2e70f
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-12 08:26:08 -0600

    Codigo formateado

--- src/constants/apiEndpoints.js
+++ src/constants/apiEndpoints.js
@@ -1,25 +1,26 @@
 export const API_ENDPOINTS = {
-    AUTH: {
-        REGISTER: "/Auth/Register",
-        LOGIN: "/Auth/Login",
-        LOGOUT: "/Auth/Logout"
-    },
-    INTERNALNEWS: {
-        REGISTER: "/InternalNew/CreateInternalNew",
-        UPDATE: "/InternalNew/UpdateInteranlNew/",
-        DELETE: "/InternalNew/DeleteInternalNew/",
-        GETINTERNALNEWS: "/InternalNew/GetInternalNews",
-        GETINTERNALNEWSBYID: "/InternalNew/GetInternalNewsById/"
-    },
-    FEATUREDCOLLABORATORS: {
-        REGISTER: "/FeaturedCollaborators/CreateFeaturedCollaborators",
-        GETCOLLABORATOR: "/FeaturedCollaborators/GetCollaborators"
-    },
-    BLOGCONTENT: {
-        GETBLOGCONTENT: "/BlogContent/GetBlogContent",
-        GETBLOGCONTENTBYID: "/BlogContent/GetBlogContentById/",
-        REGISTER: "/BlogContent/Register",
-        UPDATE: "/BlogContent/Edit/",
-        DELETE: "/BlogContent/Delete/"
-    }
+  AUTH: {
+    REGISTER: "/Auth/Register",
+    LOGIN: "/Auth/Login",
+    LOGOUT: "/Auth/Logout",
+  },
+  INTERNALNEWS: {
+    REGISTER: "/InternalNew/CreateInternalNew",
+    UPDATE: "/InternalNew/UpdateInteranlNew/",
+    DELETE: "/InternalNew/DeleteInternalNew/",
+    GETINTERNALNEWS: "/InternalNew/GetInternalNews",
+    GETINTERNALNEWSBYID: "/InternalNew/GetInternalNewsById/",
+  },
+  FEATUREDCOLLABORATORS: {
+    REGISTER: "/FeaturedCollaborators/CreateFeaturedCollaborators",
+    GETCOLLABORATOR: "/FeaturedCollaborators/GetCollaborators",
+  },
+  BLOGCONTENT: {
+    GETBLOGCONTENT: "/BlogContent/GetBlogContent",
+    GETALLBLOGCONTENT: "/BlogContent/GetAllBlogContent",
+    GETBLOGCONTENTBYID: "/BlogContent/GetBlogContentById/",
+    REGISTER: "/BlogContent/Register",
+    UPDATE: "/BlogContent/Edit/",
+    DELETE: "/BlogContent/Delete/",
+  },
 };
