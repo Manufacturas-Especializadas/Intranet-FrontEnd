7ad1d431cd354ed975f44b9bbc83a9e279a609cf
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-12 08:26:24 -0600

    Codigo formateado

--- src/api/services/blogContentService.js
+++ src/api/services/blogContentService.js
@@ -1,19 +1,27 @@
 import { API_ENDPOINTS } from "../../constants/apiEndpoints";
 import httpClient from "../httpClient";
 
-
 const blogContentService = {
-    blogContent: (pageType) => {
-        return httpClient.get(`${API_ENDPOINTS.BLOGCONTENT.GETBLOGCONTENT}?pageType=${encodeURIComponent(pageType)}`);
-    },
-    blogContenById: (id, pageType) => {
-        return httpClient.get(`${API_ENDPOINTS.BLOGCONTENT.GETBLOGCONTENTBYID}${id}?pageType=${encodeURIComponent(pageType)}`);
-    },
-    register: (formData) => httpClient.post(API_ENDPOINTS.BLOGCONTENT.REGISTER, formData),
-    update: (id, formData) => {
-        return httpClient.put(`${API_ENDPOINTS.BLOGCONTENT.UPDATE}${id}`, formData);
-    },
-    delete: (id) => httpClient.delete(`${API_ENDPOINTS.BLOGCONTENT.DELETE}${id}`)
-}
+  blogContent: (pageType) => {
+    return httpClient.get(
+      `${
+        API_ENDPOINTS.BLOGCONTENT.GETBLOGCONTENT
+      }?pageType=${encodeURIComponent(pageType)}`
+    );
+  },
+  blogContenById: (id, pageType) => {
+    return httpClient.get(
+      `${
+        API_ENDPOINTS.BLOGCONTENT.GETBLOGCONTENTBYID
+      }${id}?pageType=${encodeURIComponent(pageType)}`
+    );
+  },
+  register: (formData) =>
+    httpClient.post(API_ENDPOINTS.BLOGCONTENT.REGISTER, formData),
+  update: (id, formData) => {
+    return httpClient.put(`${API_ENDPOINTS.BLOGCONTENT.UPDATE}${id}`, formData);
+  },
+  delete: (id) => httpClient.delete(`${API_ENDPOINTS.BLOGCONTENT.DELETE}${id}`),
+};
 
 export default blogContentService;
