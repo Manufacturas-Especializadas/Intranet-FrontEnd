7ea84b1f42c344aca2be0050833e19fa1e03bf3b
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-12 11:00:24 -0600

    Codigo formateado

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,23 @@
-import Calendario from "../../components/Calendario/Calendario"
-import { EnlacesRapidos } from "../../components/EnlacesRapidos/EnlacesRapidos"
-import EquipoDestacado from "../../components/EquipoDestacado/EquipoDestacado"
-import Footer from "../../components/Footer/Footer"
-import Hero from "../../components/Hero/Hero"
-import Noticias from "../../components/Noticias/Noticias"
-import Valores from "../../components/Valores/Valores"
+import Calendario from "../../components/Calendario/Calendario";
+import { EnlacesRapidos } from "../../components/EnlacesRapidos/EnlacesRapidos";
+import EquipoDestacado from "../../components/EquipoDestacado/EquipoDestacado";
+import Footer from "../../components/Footer/Footer";
+import Hero from "../../components/Hero/Hero";
+import Noticias from "../../components/Noticias/Noticias";
+import Valores from "../../components/Valores/Valores";
 
 const Home = () => {
-    return (
-        <>
-            <Hero />
-            <Noticias />
-            <EnlacesRapidos />
-            <Valores />
-            <Calendario />
-            <EquipoDestacado />
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <Hero />
+      <Noticias />
+      <EnlacesRapidos />
+      <Valores />
+      <Calendario />
+      <EquipoDestacado />
+      <Footer />
+    </>
+  );
+};
 
-export default Home
+export default Home;
