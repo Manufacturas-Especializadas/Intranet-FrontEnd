908d1b9ce916e9796436ab4955b71d848f026ec5
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-13 08:29:21 -0600

    Se quito el componente de equipo destacado

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
       <EnlacesRapidos />
       <Valores />
       <Calendario />
-      <EquipoDestacado />
+      {/* <EquipoDestacado /> */}
       <Footer />
     </>
   );
